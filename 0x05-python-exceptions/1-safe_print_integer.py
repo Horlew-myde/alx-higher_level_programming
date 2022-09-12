78eb484182454ef10113b8aaa5e0a0da170b3511
Horlew-myde <mezamune@gmail.com>
2022-09-12 13:15:39 -0400

    question 1 hotfix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except Error:
+    except None:
         return False
-#    else:
-#        return (True)
+    else:
+        return True
