496b61c9d213bfada2758d38a73fdc4a8f26ae2a
Horlew-myde <mezamune@gmail.com>
2022-09-27 12:31:20 -0400

    question 0,1,2,3,4 hot fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-""" Module that contains a function that writes to a text file
+
 """
+This module defines a string-to-JSON function.
+"""
+import json
 
 
-def write_file(filename="", text=""):
-    """ Function that writes to a text file
-
-    Args:
-        filename: filename
-        text: text to write
-
-    Raises
-        Exception: when the file can be opened
-
+def to_json_string(my_obj):
     """
-
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+    Return the JSON representation of a string object.
+    """
+    return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-""" Module that contains a function that appends to a text file
 """
+This module defines a JSON-to-object function.
+"""
+import json
 
 
-def append_write(filename="", text=""):
-    """ Function that appends to a text file
-
-    Args:
-        filename: filename
-        text: text to write
-
-    Raises
-        Exception: when the file can be opened
-
+def from_json_string(my_str):
     """
-
-    with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+    Return the Python object representation of a JSON string.
+    """
+    return json.loads(my_str)
