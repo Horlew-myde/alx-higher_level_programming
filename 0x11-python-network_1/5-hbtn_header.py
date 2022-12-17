898a429f2ec51855032d824e4d96a4c58f76629e
Horlew-myde <mezamune@gmail.com>
2022-12-17 18:04:11 -0500

    numero 5 hot fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,12 +3,15 @@
 Python script that sends a request to the URL and
 displays the value of a variable in the response header
 """
-import requests
+
 import sys
+import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    print(r.headers.get("X-Request-Id"))
+
+
