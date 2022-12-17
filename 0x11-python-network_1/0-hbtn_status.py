194ec95431cc643e9f674cde8684fee32dcf1b02
Horlew-myde <mezamune@gmail.com>
2022-12-17 17:52:34 -0500

    numero 0 hot fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
